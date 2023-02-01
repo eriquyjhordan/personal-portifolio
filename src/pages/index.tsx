d8b2be0fdbe1c25501daffeb0c04c6b5ccb04e04
Eriquy Jhordan <eriquy@gmail.com>
2023-02-01 15:47:17 -0300

    feat: create header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,48 @@
 import { Inter } from '@next/font/google'
+import Link from 'next/link'
 
-// import Logo from '../../public/eriquy-jhordan-logo.svg'
+import Logo from '../../public/eriquyjhordan.svg'
+import GitHub from '../../public/icons/github.png'
+import LinkedIn from '../../public/icons/linkedin.png'
+import Mail from '../../public/icons/mail.png'
 
 const inter = Inter({
   variable: '--inter',
   subsets: ['latin']
 })
 
-import { Header, LogoWrapper } from '../styles/index'
+import { Container, Wrapper, Header, NavWrapper, Nav, IconsWrapper, Icon, Circle } from '../styles/index'
 
 export default function Home() {
   return (
     <>
-      <div className={inter.className}>
-        <Header >
-        </Header>
-
-      </div>
+      <Container>
+        <Wrapper>
+          <Circle />
+          <div className={inter.className}>
+            <Header >
+              <Logo width={300} />
+              <NavWrapper>
+                <Nav href="/">Início</Nav>
+                <Nav href="/">Sobre</Nav>
+                <Nav href="/">Projetos</Nav>
+                <Nav href="/">Contatos</Nav>
+              </NavWrapper>
+              <IconsWrapper>
+                <Link href="https://www.github.com/eriquyjhordan" target="_blank">
+                  <Icon src={GitHub} alt="github icon and link" width={24} height={24} />
+                </Link>
+                <Link href="https://www.linkedin.com/in/eriquyjhordan" target="_blank">
+                  <Icon src={LinkedIn} alt="Linkedin icon and link" width={24} height={24} />
+                </Link>
+                <Link href="">
+                  <Icon src={Mail} alt="Mail icon and link" width={24} height={24} />
+                </Link>
+              </IconsWrapper>
+            </Header>
+          </div>
+        </Wrapper>
+      </Container>
     </>
   )
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,18 +1,61 @@
 import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
 
-export const Title = styled.h1`
-  color: black;
-  font-size: 24px;
-  font-weight: 400;
+export const Container = styled.div`
+  width: 100%;
+  background-color: #f5f5f5;
+  height: 100vh;
+`;
+
+export const Wrapper = styled.div`
+  max-width: 1216px;
+  margin: 0 auto;
+  justify-content: center;
+  padding: 0 24px;
 `;
 
 export const Header = styled.header`
-  height: 77px;
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 24px;
 `;
 
-export const LogoWrapper = styled.div`
-  > img {
-    height: 100%;
-  }
+export const NavWrapper = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 32px;
+  position: relative;
+`;
+export const Nav = styled(Link)`
+  color: #0f1221;
+  font-size: 18px;
+  line-height: 22px;
+  font-weight: 400;
+  text-decoration: none;
+`;
+export const IconsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 32px;
+`;
+export const Icon = styled(Image)`
+  width: 24px;
+  height: 24px;
+`;
+
+export const Circle = styled.div`
+  position: absolute;
+  left: 0%;
+  top: auto;
+  right: 0%;
+  bottom: 0%;
+  width: 300%;
+  height: 300%;
+  margin-left: -100%;
+  border-radius: 100%;
+  background-color: #edf2ff;
+  z-index: -1;
 `;
