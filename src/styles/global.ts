71d5bf38197609d4f863a3e9a2788283064780b3
Eriquy Jhordan <eriquy@gmail.com>
2023-02-02 12:59:43 -0300

    fix: ajusts to responsive design
    Ajust px to rem and change to 1rem for equal to 10px

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,6 +6,17 @@ export default createGlobalStyle`
         padding: 0;
         outline:0;
         box-sizing: border-box;
+        font-size: 62.5%;
+        @media (max-width: 1080px){
+            font-size: 56.25%;
+        }
+        @media (max-width: 720px){
+            font-size: 50%;
+        }
+        
+    }
+    body{
+        background-color: #f5f5f5;
     }
     #root{
         margin:0 auto;
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,15 +4,13 @@ import Image from "next/image";
 
 export const Container = styled.div`
   width: 100%;
-  background-color: #f5f5f5;
-  height: 100vh;
 `;
 
 export const Wrapper = styled.div`
-  max-width: 1216px;
+  max-width: 121.6rem;
   margin: 0 auto;
   justify-content: center;
-  padding: 0 24px;
+  padding: 0 2.4rem;
 `;
 
 export const Header = styled.header`
@@ -20,19 +18,19 @@ export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-top: 24px;
+  padding-top: 2.4rem;
 `;
 
 export const NavWrapper = styled.nav`
   display: flex;
   align-items: center;
-  gap: 32px;
+  gap: 3.2rem;
   position: relative;
 `;
 export const Nav = styled(Link)`
   color: #0f1221;
-  font-size: 18px;
-  line-height: 22px;
+  font-size: 1.8rem;
+  line-height: 2.2rem;
   font-weight: 400;
   text-decoration: none;
   cursor: pointer;
@@ -40,17 +38,17 @@ export const Nav = styled(Link)`
   &:hover {
     color: #0f1221;
     opacity: 0.8;
-    border-bottom: 1px solid #0f1221;
+    border-bottom: 0.1rem solid #0f1221;
   }
 `;
 export const IconsWrapper = styled.div`
   display: flex;
   align-items: center;
-  gap: 32px;
+  gap: 3.2rem;
 `;
 export const Icon = styled(Image)`
-  width: 24px;
-  height: 24px;
+  width: 2.4rem;
+  height: 2.4rem;
   cursor: pointer;
   &:hover {
     opacity: 0.8;
@@ -59,32 +57,36 @@ export const Icon = styled(Image)`
 
 export const Hero = styled.section`
   width: 100%;
-  margin-top: 80px;
+  margin-top: 8rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  gap: 80px;
+  gap: 1rem;
 `;
 export const HeroLeft = styled.div``;
 export const Title = styled.h1`
-  max-width: 420px;
+  max-width: 42rem;
   font-weight: 700;
   color: #0f1221;
-  line-height: 56px;
-  font-size: 46px;
+  line-height: 5.6rem;
+  font-size: 4.6rem;
 `;
 export const SubTitle = styled.h2`
   font-weight: 700;
   color: #5e3aee;
-  line-height: 47px;
-  font-size: 38px;
+  line-height: 4.7rem;
+  font-size: 3.8rem;
 `;
 export const Description = styled.h3`
   font-weight: 400;
-  max-width: 450px;
-  margin-top: 24px;
+  max-width: 45rem;
+  margin: 2.4rem 0;
   color: #0f1221;
-  margin-bottom: 36px;
+  font-size: 1.8rem;
+  & strong {
+    font-weight: 700;
+    font-size: 1.8rem;
+  }
 `;
 export const ButtonCalltoAction = styled(Link)`
   background-color: #4263eb;
@@ -92,25 +94,34 @@ export const ButtonCalltoAction = styled(Link)`
   font-weight: 600;
   font-size: 18px;
   line-height: 150%;
-  box-shadow: 0px 4px 4px rgba(15, 18, 33, 0.3);
-  border-radius: 8px;
-  width: 300px;
-  height: 56px;
-  padding: 16px 60px;
+  box-shadow: 0rem 0.4rem 0.4rem rgba(15, 18, 33, 0.3);
+  border-radius: 0.8rem;
+  width: 30rem;
+  height: 5rem;
+  padding: 0.8rem 6rem;
   text-decoration: none;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    opacity: 0.85;
+  }
 `;
 export const ButtonDisclaimer = styled.p`
-  margin-top: 8px;
+  margin-top: 0.8rem;
   font-weight: 400;
-  font-size: 10px;
-  line-height: 12px;
+  font-size: 1rem;
+  line-height: 1.2rem;
   color: #868e96;
-  max-width: 300px;
+  max-width: 30rem;
   text-align: center;
 `;
 export const HeroRight = styled.div``;
-export const ImageHero = styled(Image)``;
+export const ImageHero = styled(Image)`
+  width: 100%;
+  object-fit: contain;
+`;
 export const SubtitleWrapper = styled.div`
   display: flex;
   align-items: center;
