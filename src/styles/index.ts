a0d3787d415b0671d97bc555729d8027ef763932
ERIQUY JHORDAN <eriquy@gmail.com>
2023-02-02 18:06:02 +0000

    feat: create medias queries to  adapt to mobile devices

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -21,11 +21,31 @@ export const Header = styled.header`
   padding-top: 2.4rem;
 `;
 
+export const LogoWrapper = styled.div`
+  & svg {
+    font-size: 6rem;
+  }
+  @media (max-width: 1080px) {
+    & svg {
+      font-size: 8rem;
+    }
+  }
+  @media (max-width: 720px) {
+    & svg {
+      font-size: 12rem;
+      max-width: 90%;
+    }
+  }
+`;
+
 export const NavWrapper = styled.nav`
   display: flex;
   align-items: center;
   gap: 3.2rem;
   position: relative;
+  @media (max-width: 790px) {
+    display: none;
+  }
 `;
 export const Nav = styled(Link)`
   color: #0f1221;
@@ -45,6 +65,9 @@ export const IconsWrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 3.2rem;
+  @media (max-width: 790px) {
+    display: none;
+  }
 `;
 export const Icon = styled(Image)`
   width: 2.4rem;
@@ -55,6 +78,19 @@ export const Icon = styled(Image)`
   }
 `;
 
+export const HamburguerIcon = styled(Image)`
+  display: none;
+  @media (max-width: 790px) {
+    display: block;
+    width: 3.2rem;
+    height: 3.2rem;
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
 export const Hero = styled.section`
   width: 100%;
   margin-top: 8rem;
@@ -62,14 +98,32 @@ export const Hero = styled.section`
   justify-content: space-between;
   align-items: center;
   gap: 1rem;
+  @media (max-width: 790px) {
+    flex-direction: column-reverse;
+    justify-content: center;
+    gap: 0;
+  }
 `;
-export const HeroLeft = styled.div``;
+export const HeroLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 790px) {
+    align-items: center;
+    margin-top: 4rem;
+  }
+`;
 export const Title = styled.h1`
   max-width: 42rem;
   font-weight: 700;
   color: #0f1221;
   line-height: 5.6rem;
   font-size: 4.6rem;
+  @media (max-width: 790px) {
+    font-size: 3.6rem;
+    line-height: 4.6rem;
+    text-align: center;
+    max-width: 28.4rem;
+  }
 `;
 export const SubTitle = styled.h2`
   font-weight: 700;
@@ -87,12 +141,15 @@ export const Description = styled.h3`
     font-weight: 700;
     font-size: 1.8rem;
   }
+  @media (max-width: 790px) {
+    text-align: center;
+  }
 `;
 export const ButtonCalltoAction = styled(Link)`
   background-color: #4263eb;
   color: #f9f9fb;
   font-weight: 600;
-  font-size: 18px;
+  font-size: 1.8rem;
   line-height: 150%;
   box-shadow: 0rem 0.4rem 0.4rem rgba(15, 18, 33, 0.3);
   border-radius: 0.8rem;
@@ -121,6 +178,11 @@ export const HeroRight = styled.div``;
 export const ImageHero = styled(Image)`
   width: 100%;
   object-fit: contain;
+  @media (max-width: 790px) {
+    width: 100%;
+    height: 100%;
+    max-width: 48rem;
+  }
 `;
 export const SubtitleWrapper = styled.div`
   display: flex;
